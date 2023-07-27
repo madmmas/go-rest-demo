c66fcd7bbfea7849c14196954e30bd6d9f922c24
Moinuddin Masud <moinuddinmasud@gmail.com>
2023-07-27 18:50:30 +0100

    App Run method added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// App - the struct holds the db connection
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up the app")
+	return nil
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Demo REST API!")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
